f74844578e111bd190f2fdc5685c4564fcf7ab0f
LucasLvy <lucaslevy10@gmail.com>
2021-05-20 18:57:33 +0200

    Tests and errors

--- contract/smartpy/FA12_staking.py
+++ contract/smartpy/FA12_staking.py
@@ -1,4 +1,16 @@
 import smartpy as sp
+class Error:
+    def make(s): return ("Staking " + s)
+
+    NotAdmin                        = make("Access denied")
+    AmountTooHigh                   = make("Amount is too high")
+    AmountTooLow                    = make("Amount is too low")
+    DupStakingOpt                   = make("A staking option with this Id already exists")
+    NotStakingOpt                   = make("The staking option for this id does not exist")
+    NeverStaked                     = make("Never staked on this contract")
+    NeverUsedPack                   = make("Never staked using this pack")
+    NotStaking                      = make("This stake doesn't exist")
+
 
 Stake = sp.TRecord(
     timestamp=sp.TTimestamp,
@@ -47,26 +59,26 @@ class FA12Staking(sp.Contract):
     @sp.entry_point
     def updateReserve(self, params):
         sp.set_type(params, sp.TRecord(reserve=sp.TAddress))
-        sp.verify_equal(sp.sender, self.data.admin, "Access denied")
+        sp.verify_equal(sp.sender, self.data.admin, Error.NotAdmin)
         self.data.reserve = params.reserve
 
     @sp.entry_point
     def updateAdmin(self, params):
         sp.set_type(params, sp.TRecord(admin=sp.TAddress))
-        sp.verify_equal(sp.sender, self.data.admin, "Access denied")
+        sp.verify_equal(sp.sender, self.data.admin, Error.NotAdmin)
         self.data.admin = params.admin
         
     @sp.entry_point
     def updateContract(self, params):
         sp.set_type(params, sp.TRecord(contract = sp.TAddress))
-        sp.verify_equal(sp.sender, self.data.admin, "Access denied")
+        sp.verify_equal(sp.sender, self.data.admin, Error.NotAdmin)
         self.data.FA12TokenContract = params.contract
     
     
     @sp.entry_point
     def updateVotingContract(self, params):
         sp.set_type(params, sp.TRecord(contract = sp.TAddress))
-        sp.verify_equal(sp.sender, self.data.admin, "Access denied")
+        sp.verify_equal(sp.sender, self.data.admin, Error.NotAdmin)
         self.data.votingContract = sp.some(params.contract)
     
     @sp.sub_entry_point
@@ -79,8 +91,8 @@ class FA12Staking(sp.Contract):
     @sp.entry_point
     def createStakingOption(self, params):
         sp.set_type(params, sp.TRecord(_id = sp.TNat, rate = sp.TNat, _max = sp.TNat, _min = sp.TNat, duration = sp.TInt).layout(("_id as id", ("rate", ("_max as max", ("_min as min", "duration"))))))
-        sp.verify(~self.data.stakingOptions.contains(params._id), "A staking option with this Id already exists")
-        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), "Access denied")
+        sp.verify(~self.data.stakingOptions.contains(params._id), Error.DupStakingOpt)
+        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), Error.NotAdmin)
         option = sp.record(
             minStake = params._min,
             maxStake = params._max,
@@ -92,57 +104,66 @@ class FA12Staking(sp.Contract):
     @sp.entry_point
     def updateStakingOptionRate(self, params):
         sp.set_type(params, sp.TRecord(_id = sp.TNat, rate = sp.TNat).layout(("_id as id", "rate")))
-        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), "Access denied")
-        sp.verify(self.data.stakingOptions.contains(params._id), "The staking option for this id does not exist")
+        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), Error.NotAdmin)
+        sp.verify(self.data.stakingOptions.contains(params._id), Error.NotStakingOpt)
         self.data.stakingOptions[params._id].stakingPercentage = params.rate
 
     @sp.entry_point
     def updateStakingOptionDuration(self, params):
         sp.set_type(params, sp.TRecord(_id = sp.TNat, duration = sp.TInt).layout(("_id as id", "duration")))
-        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), "Access denied")
-        sp.verify(self.data.stakingOptions.contains(params._id), "The staking option for this id does not exist")
+        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), Error.NotAdmin)
+        sp.verify(self.data.stakingOptions.contains(params._id), Error.NotStakingOpt)
         self.data.stakingOptions[params._id].stakingPeriod = params.duration
         
     @sp.entry_point
     def updateStakingOptionMax(self, params):
         sp.set_type(params, sp.TRecord(_id = sp.TNat, _max = sp.TNat).layout(("_id as id", "_max as max")))
-        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), "Access denied")
-        sp.verify(self.data.stakingOptions.contains(params._id), "The staking option for this id does not exist")
+        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), Error.NotAdmin)
+        sp.verify(self.data.stakingOptions.contains(params._id), Error.NotStakingOpt)
         self.data.stakingOptions[params._id].maxStake = params._max
         
     @sp.entry_point
     def updateStakingOptionMin(self, params):
         sp.set_type(params, sp.TRecord(_id = sp.TNat, _min = sp.TNat).layout(("_id as id", "_min as min")))
-        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), "Access denied")
-        sp.verify(self.data.stakingOptions.contains(params._id), "The staking option for this id does not exist")
+        sp.verify(self.is_voting_contract(sp.sender) | (self.data.admin == sp.sender), Error.NotAdmin)
+        sp.verify(self.data.stakingOptions.contains(params._id), Error.NotStakingOpt)
         self.data.stakingOptions[params._id].minStake = params._min
-        
+    
+    def initUserStaking(self, addr, pack, staking):
+        self.data.userStakePack[addr] = sp.map({pack:sp.map({0:staking})})
+    
+    def addStaking(self, addr, pack, staking):
+        self.data.userStakePack[addr][pack][sp.as_nat(sp.len(self.data.userStakePack[addr][pack])-1)]= staking
+    
+    def addStakingPack(self, addr, pack, staking):
+        self.data.userStakePack[addr][pack]= sp.map({0:staking})
+    
     @sp.entry_point
     def stake(self, params):
+        sp.trace(sp.now)
         sp.set_type(params, sp.TRecord(pack = sp.TNat, amount = sp.TNat))
-        sp.verify(params.amount > sp.nat(0), "failed")
+        sp.verify(params.amount > self.data.stakingOptions[params.pack].minStake, Error.AmountTooLow)
+        sp.verify(params.amount < self.data.stakingOptions[params.pack].maxStake, Error.AmountTooHigh)
         staking = sp.record(timestamp=sp.now, rate = self.data.stakingOptions[params.pack].stakingPercentage, value = params.amount)
-        p = sp.nat(0)
-        sp.if self.data.userStakePack.contains(sp.sender):
-            sp.trace(self.data.userStakePack)
-            sp.if self.data.userStakePack[sp.sender].contains(params.pack):
-                sp.trace(sp.len(self.data.userStakePack[sp.sender][params.pack]))
-                j = sp.len(self.data.userStakePack[sp.sender][params.pack]) # TODO : trouver le problème
-        sp.trace(p) 
-        stakingIndex = sp.map({p:staking})
-        stakingPack = sp.map({params.pack:stakingIndex})
-        self.data.userStakePack[sp.sender]= stakingPack
+        sp.if ~self.data.userStakePack.contains(sp.sender):
+            self.initUserStaking(sp.sender, params.pack, staking)
+        sp.else:
+            sp.if ~self.data.userStakePack[sp.sender].contains(params.pack):
+                self.addStakingPack(sp.sender, params.pack, staking)
+            sp.else:
+                self.addStaking(sp.sender, params.pack, staking)
+
 
     @sp.entry_point
     def unstakeLock(self, params):
         """pack=sp.TNat,"""
         sp.set_type(params, sp.TRecord(pack=sp.TNat, index=sp.TNat))
         """ on vérifie que le sender a bien deja staké """
-        sp.verify(self.data.userStakePack.contains(sp.sender))
+        sp.verify(self.data.userStakePack.contains(sp.sender), Error.NeverStaked)
         """ on vérifie que le sender a deja staké le pack qu'il veut redeem """
-        sp.verify(self.data.userStakePack[sp.sender].contains(params.pack))
+        sp.verify(self.data.userStakePack[sp.sender].contains(params.pack), Error.NeverUsedPack)
         """ on vérifie que le staking qu'il veut withdraw existe """
-        sp.verify(sp.len(self.data.userStakePack[sp.sender][params.pack]) > params.index)
+        sp.verify(sp.len(self.data.userStakePack[sp.sender][params.pack]) > params.index, Error.NotStaking)
         amount = sp.nat(0)
         sp.if (self.data.userStakePack[sp.sender][params.pack][params.index].timestamp.add_days(self.data.stakingOptions[params.pack].stakingPeriod) > sp.now):
             amount = self.getReward(self.data.userStakePack[sp.sender][params.pack][params.index], self.data.userStakePack[sp.sender][params.pack][params.index].timestamp.add_days(self.data.stakingOptions[params.pack].stakingPeriod)) + self.data.userStakePack[sp.sender][params.pack][params.index].value
@@ -158,15 +179,16 @@ class FA12Staking(sp.Contract):
     def unstakeFlex(self, params):
         sp.set_type(params, sp.TNat)
         """ on vérifie que le sender a bien deja staké """
-        sp.verify(self.data.userStakePack.contains(sp.sender))
+        sp.verify(self.data.userStakePack.contains(sp.sender), Error.NeverStaked)
         """ on vérifie que le sender a deja staké le pack qu'il veut redeem """
-        sp.verify(self.data.userStakePack[sp.sender][0].contains(params))
+        sp.verify(self.data.userStakePack[sp.sender][0].contains(params), Error.NeverUsedPack)
 
         paramTrans = sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat).layout(("from_ as from", ("to_ as to", "value")))
         paramCall = sp.record(from_=self.data.reserve, to_=sp.sender, value=self.getReward(self.data.userStakePack[sp.sender][0][params], sp.now))
         call(sp.contract(paramTrans ,self.data.FA12TokenContract ,entry_point="transfer").open_some(), paramCall)
-        self.data.userStakePack[sp.sender][0][params] = self.data.userStakePack[sp.sender][0][sp.as_nat(sp.len(self.data.userStakePack[sp.sender][0])-1)]
-        del self.data.userStakePack[sp.sender][0][sp.as_nat(sp.len(self.data.userStakePack[sp.sender][0])-1)]
+        """self.data.userStakePack[sp.sender][0][params] = self.data.userStakePack[sp.sender][0][sp.as_nat(sp.len(self.data.userStakePack[sp.sender][0])-1)]
+        del self.data.userStakePack[sp.sender][0][sp.as_nat(sp.len(self.data.userStakePack[sp.sender][0])-1)]"""
+        del self.data.userStakePack[sp.sender][0][params]
 
     def getReward(self, stake, end):
         k = sp.nat(10000)
@@ -216,12 +238,14 @@ def test():
     scenario += c1.createStakingOption(_id = 0, rate = 20, _max = 1000000000000, _min = 0, duration = 0).run(sender = alice, valid = False)
     scenario.h3("Admin creates a new staking option")
     scenario += c1.createStakingOption(_id = 0, rate = 20, _max = 1000000000000, _min = 0, duration = 0).run(sender = admin)
+    scenario.h3("Admin creates a new staking option")
+    scenario += c1.createStakingOption(_id = 1, rate = 20, _max = 1000000000000, _min = 0, duration = 100000000).run(sender = admin)
     
     scenario.h2("Updating MaxStake")
     scenario.h3("Alice tries to update the state variable but does not succeed")
-    scenario += c1.updateStakingOptionMax(_id = 0, _max = 10000).run(sender = alice, valid = False)
+    scenario += c1.updateStakingOptionMax(_id = 0, _max = 1000000).run(sender = alice, valid = False)
     scenario.h3("Admin updates the state variable")
-    scenario += c1.updateStakingOptionMax(_id = 0, _max = 10000).run(sender = admin)
+    scenario += c1.updateStakingOptionMax(_id = 0, _max = 1000000).run(sender = admin)
 
     scenario.h2("Updating MinStake")
     scenario += c1.updateStakingOptionMin(_id = 0, _min = 1000).run(sender = alice, valid = False)
@@ -245,7 +269,17 @@ def test():
     scenario += c1.updateAdmin(admin=alice.address).run(sender=admin)
 
     scenario.h2("Staking")
-    scenario.h3("Alice tries to stake and succeeds")
-    scenario += c1.stake(pack=0, amount = 10000).run(sender=alice)
+    scenario.h3("Alice tries to stake flex and succeeds")
+    scenario += c1.stake(pack=0, amount = 100).run(sender=alice)
     scenario.h3("Alice tries to stake on the same pack")
-    scenario += c1.stake(pack=0, amount = 10000).run(sender=alice)
+    scenario += c1.stake(pack=0, amount = 100000).run(sender=alice)
+    scenario.h3("Alice tries to stake Lock and succeeds")
+    scenario += c1.stake(pack = 1, amount = 100000).run(sender=alice)
+    
+    #scenario.h2("Unstaking")
+    scenario.h3("Alice tries to unstake and succeeds")
+    scenario += c1.unstakeFlex(0).run(sender=alice)
+    scenario.h3("Alice tries to unstake a pack she didn't stake and fails")
+    scenario += c1.unstakeFlex(1).run(sender=alice, valid = False)
+    scenario.h3("Alice tries to unstake a lock pack and succeeds")
+    scenario += c1.unstakeLock(pack = 1, index = 0).run(sender= alice)
